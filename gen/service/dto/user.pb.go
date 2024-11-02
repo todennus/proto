b7d0e84e2368f3b6697a5b6f65f331ed5593aac1
huykingsofm <44906193+huykingsofm@users.noreply.github.com>
2024-11-03 00:11:47 +0700

    add file service (#5)

--- gen/service/dto/user.pb.go
+++ gen/service/dto/user.pb.go
@@ -209,6 +209,112 @@ func (x *UserGetByIDResponse) GetUser() *resource.User {
 	return nil
 }
 
+type UserValidateAvatarPolicyTokenRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	PolicyToken string `protobuf:"bytes,1,opt,name=policy_token,json=policyToken,proto3" json:"policy_token,omitempty"`
+}
+
+func (x *UserValidateAvatarPolicyTokenRequest) Reset() {
+	*x = UserValidateAvatarPolicyTokenRequest{}
+	mi := &file_dto_user_proto_msgTypes[4]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *UserValidateAvatarPolicyTokenRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UserValidateAvatarPolicyTokenRequest) ProtoMessage() {}
+
+func (x *UserValidateAvatarPolicyTokenRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_dto_user_proto_msgTypes[4]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UserValidateAvatarPolicyTokenRequest.ProtoReflect.Descriptor instead.
+func (*UserValidateAvatarPolicyTokenRequest) Descriptor() ([]byte, []int) {
+	return file_dto_user_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *UserValidateAvatarPolicyTokenRequest) GetPolicyToken() string {
+	if x != nil {
+		return x.PolicyToken
+	}
+	return ""
+}
+
+type UserValidateAvatarPolicyTokenResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	UserId       int64    `protobuf:"varint,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
+	AllowedTypes []string `protobuf:"bytes,2,rep,name=allowed_types,json=allowedTypes,proto3" json:"allowed_types,omitempty"`
+	MaxSize      int32    `protobuf:"varint,3,opt,name=max_size,json=maxSize,proto3" json:"max_size,omitempty"`
+}
+
+func (x *UserValidateAvatarPolicyTokenResponse) Reset() {
+	*x = UserValidateAvatarPolicyTokenResponse{}
+	mi := &file_dto_user_proto_msgTypes[5]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *UserValidateAvatarPolicyTokenResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UserValidateAvatarPolicyTokenResponse) ProtoMessage() {}
+
+func (x *UserValidateAvatarPolicyTokenResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_dto_user_proto_msgTypes[5]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UserValidateAvatarPolicyTokenResponse.ProtoReflect.Descriptor instead.
+func (*UserValidateAvatarPolicyTokenResponse) Descriptor() ([]byte, []int) {
+	return file_dto_user_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *UserValidateAvatarPolicyTokenResponse) GetUserId() int64 {
+	if x != nil {
+		return x.UserId
+	}
+	return 0
+}
+
+func (x *UserValidateAvatarPolicyTokenResponse) GetAllowedTypes() []string {
+	if x != nil {
+		return x.AllowedTypes
+	}
+	return nil
+}
+
+func (x *UserValidateAvatarPolicyTokenResponse) GetMaxSize() int32 {
+	if x != nil {
+		return x.MaxSize
+	}
+	return 0
+}
+
 var File_dto_user_proto protoreflect.FileDescriptor
 
 var file_dto_user_proto_rawDesc = []byte{
@@ -232,11 +338,24 @@ var file_dto_user_proto_rawDesc = []byte{
 	0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x35, 0x0a, 0x04, 0x75, 0x73, 0x65, 0x72,
 	0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x21, 0x2e, 0x74, 0x6f, 0x64, 0x65, 0x6e, 0x6e, 0x75,
 	0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2e, 0x64, 0x74, 0x6f, 0x2e, 0x72, 0x65, 0x73, 0x6f,
-	0x75, 0x72, 0x63, 0x65, 0x2e, 0x55, 0x73, 0x65, 0x72, 0x52, 0x04, 0x75, 0x73, 0x65, 0x72, 0x42,
-	0x2b, 0x5a, 0x29, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x74, 0x6f,
-	0x64, 0x65, 0x6e, 0x6e, 0x75, 0x73, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x67, 0x65, 0x6e,
-	0x2f, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2f, 0x64, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x75, 0x72, 0x63, 0x65, 0x2e, 0x55, 0x73, 0x65, 0x72, 0x52, 0x04, 0x75, 0x73, 0x65, 0x72, 0x22,
+	0x49, 0x0a, 0x24, 0x55, 0x73, 0x65, 0x72, 0x56, 0x61, 0x6c, 0x69, 0x64, 0x61, 0x74, 0x65, 0x41,
+	0x76, 0x61, 0x74, 0x61, 0x72, 0x50, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0x54, 0x6f, 0x6b, 0x65, 0x6e,
+	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x21, 0x0a, 0x0c, 0x70, 0x6f, 0x6c, 0x69, 0x63,
+	0x79, 0x5f, 0x74, 0x6f, 0x6b, 0x65, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0b, 0x70,
+	0x6f, 0x6c, 0x69, 0x63, 0x79, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x22, 0x80, 0x01, 0x0a, 0x25, 0x55,
+	0x73, 0x65, 0x72, 0x56, 0x61, 0x6c, 0x69, 0x64, 0x61, 0x74, 0x65, 0x41, 0x76, 0x61, 0x74, 0x61,
+	0x72, 0x50, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x52, 0x65, 0x73, 0x70,
+	0x6f, 0x6e, 0x73, 0x65, 0x12, 0x17, 0x0a, 0x07, 0x75, 0x73, 0x65, 0x72, 0x5f, 0x69, 0x64, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x06, 0x75, 0x73, 0x65, 0x72, 0x49, 0x64, 0x12, 0x23, 0x0a,
+	0x0d, 0x61, 0x6c, 0x6c, 0x6f, 0x77, 0x65, 0x64, 0x5f, 0x74, 0x79, 0x70, 0x65, 0x73, 0x18, 0x02,
+	0x20, 0x03, 0x28, 0x09, 0x52, 0x0c, 0x61, 0x6c, 0x6c, 0x6f, 0x77, 0x65, 0x64, 0x54, 0x79, 0x70,
+	0x65, 0x73, 0x12, 0x19, 0x0a, 0x08, 0x6d, 0x61, 0x78, 0x5f, 0x73, 0x69, 0x7a, 0x65, 0x18, 0x03,
+	0x20, 0x01, 0x28, 0x05, 0x52, 0x07, 0x6d, 0x61, 0x78, 0x53, 0x69, 0x7a, 0x65, 0x42, 0x2b, 0x5a,
+	0x29, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x74, 0x6f, 0x64, 0x65,
+	0x6e, 0x6e, 0x75, 0x73, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x67, 0x65, 0x6e, 0x2f, 0x73,
+	0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2f, 0x64, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -251,17 +370,19 @@ func file_dto_user_proto_rawDescGZIP() []byte {
 	return file_dto_user_proto_rawDescData
 }
 
-var file_dto_user_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_dto_user_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
 var file_dto_user_proto_goTypes = []any{
-	(*UserValidateRequest)(nil),  // 0: todennus.proto.dto.UserValidateRequest
-	(*UserValidateResponse)(nil), // 1: todennus.proto.dto.UserValidateResponse
-	(*UserGetByIDRequest)(nil),   // 2: todennus.proto.dto.UserGetByIDRequest
-	(*UserGetByIDResponse)(nil),  // 3: todennus.proto.dto.UserGetByIDResponse
-	(*resource.User)(nil),        // 4: todennus.proto.dto.resource.User
+	(*UserValidateRequest)(nil),                   // 0: todennus.proto.dto.UserValidateRequest
+	(*UserValidateResponse)(nil),                  // 1: todennus.proto.dto.UserValidateResponse
+	(*UserGetByIDRequest)(nil),                    // 2: todennus.proto.dto.UserGetByIDRequest
+	(*UserGetByIDResponse)(nil),                   // 3: todennus.proto.dto.UserGetByIDResponse
+	(*UserValidateAvatarPolicyTokenRequest)(nil),  // 4: todennus.proto.dto.UserValidateAvatarPolicyTokenRequest
+	(*UserValidateAvatarPolicyTokenResponse)(nil), // 5: todennus.proto.dto.UserValidateAvatarPolicyTokenResponse
+	(*resource.User)(nil),                         // 6: todennus.proto.dto.resource.User
 }
 var file_dto_user_proto_depIdxs = []int32{
-	4, // 0: todennus.proto.dto.UserValidateResponse.user:type_name -> todennus.proto.dto.resource.User
-	4, // 1: todennus.proto.dto.UserGetByIDResponse.user:type_name -> todennus.proto.dto.resource.User
+	6, // 0: todennus.proto.dto.UserValidateResponse.user:type_name -> todennus.proto.dto.resource.User
+	6, // 1: todennus.proto.dto.UserGetByIDResponse.user:type_name -> todennus.proto.dto.resource.User
 	2, // [2:2] is the sub-list for method output_type
 	2, // [2:2] is the sub-list for method input_type
 	2, // [2:2] is the sub-list for extension type_name
@@ -280,7 +401,7 @@ func file_dto_user_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_dto_user_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
